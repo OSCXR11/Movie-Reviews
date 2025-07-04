98a17680ac8d8b864a20995d3cc317c55b0a30c3
OSCXR11 <oscaravendano@yahoo.com>
2025-07-03 22:12:48 -0700

    Update axiosConfig.js

--- Movie-FrontEnd/api/axiosConfig.js
+++ Movie-FrontEnd/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://movie-reviews-production.up.railway.app/",
+  baseURL: "https://movie-reviews-production.up.railway.app",
   headers: { "Content-Type": "application/json" },
 });
